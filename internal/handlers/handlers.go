7ce70298bed89a8eb6cd5e2acf112a0e28d2c5a0
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2025-03-30 14:38:38 +0500

    + + Websocket endpoint

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	"github.com/CloudyKit/jet/v6"
+	"github.com/gorilla/websocket"
+	"log"
 	"net/http"
 )
 
@@ -9,6 +11,42 @@ var views = jet.NewSet(
 	jet.NewOSFileSystemLoader("./html"),
 	jet.InDevelopmentMode())
 
+var upgradeConnection = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+// WsJsonResponse response send back from websocket
+type WsJsonResponse struct {
+	Action      string `json:"action"`
+	Message     string `json:"message"`
+	MessageType int    `json:"message_type"`
+}
+
+func WsEndpoint(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgradeConnection.Upgrade(w, r, nil)
+	if err != nil {
+		http.Error(w, "Error upgrading connection", http.StatusInternalServerError)
+		return
+	}
+
+	log.Println("Client connected")
+
+	var response WsJsonResponse
+	response.Message = `<em><small>Welcome to server</small></em>`
+
+	err = ws.WriteJSON(response)
+	if err != nil {
+		log.Println("Error writing JSON response:", err)
+		return
+	}
+
+}
+
+// Home - render Home page
 func Home(w http.ResponseWriter, r *http.Request) {
 	err := renderPage(w, "home.jet", nil)
 	if err != nil {
